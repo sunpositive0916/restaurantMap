54ef65b8e3d4e5d1ea641e130328c5fc93eaff89
MOON <sunpositive7@gmail.com>
2022-06-13 21:40:05 +0900

    API 조회방법 공부

--- back/src/controllers/indexController.js
+++ back/src/controllers/indexController.js
@@ -5,6 +5,33 @@ const secret = require("../../config/secret");
 
 const indexDao = require("../dao/indexDao");
 
+// 학생테이블 조회
+exports.readStudents = async function (req, res) {
+  const { studentIdx } = req.prams;
+
+  try {
+    const connection = await pool.getConnection(async (conn) => conn);
+    try {
+      const [rows] = await indexDao.selectStudents(connection, studentIdx);
+
+      return res.send({
+        result: rows,
+        isSuccess: true,
+        code: 200, // 요청 실패시 400번대 코드
+        message: "요청 성공",
+      });
+    } catch (err) {
+      logger.error(`readStudents Query error\n: ${JSON.stringify(err)}`);
+      return false;
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    logger.error(`readStudents DB Connection error\n: ${JSON.stringify(err)}`);
+    return false;
+  }
+};
+
 // 예시 코드
 exports.example = async function (req, res) {
   try {
--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -1,5 +1,24 @@
 const { pool } = require("../../config/database");
 
+exports.selectStudents = async function (connection, studentIdx) {
+  const Query = `SELECT * FROM Students where studentIdx = ?;`;
+  // const selectAllStudentsQuery = `SELECT * FROM Students;`;
+  // const selectStudentByNameQuery = `SELECT * FROM Students where studentName = ?;`;
+  const Params = [studentIdx];
+
+  // let Query = studentName ? selectStudentByNameQuery : selectAllStudentsQuery;
+
+  // if (!studentName) {
+  //   Query = selectAllStudentsQuery;
+  // } else {
+  //   Query = selectStudentByNameQuery;
+  // }
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
 exports.exampleDao = async function (connection, params) {
   const Query = `SELECT * FROM Students;`;
   const Params = [];
