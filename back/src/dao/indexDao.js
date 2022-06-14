71cfa56a8f8dee779359cdf598717bca96df02a6
MOON <sunpositive7@gmail.com>
2022-06-14 20:39:58 +0900

    post 하는 방법 공부

--- back/src/dao/indexDao.js
+++ back/src/dao/indexDao.js
@@ -1,5 +1,20 @@
 const { pool } = require("../../config/database");
 
+exports.insertStudents = async function (
+  connection,
+  studentName,
+  major,
+  birth,
+  address
+) {
+  const Query = `insert into Students(studentName, major, birth, address) values (?,?,?,?)`;
+  const Params = [studentName, major, birth, address];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
 exports.selectStudents = async function (connection, studentIdx) {
   const Query = `SELECT * FROM Students where studentIdx = ?;`;
   // const selectAllStudentsQuery = `SELECT * FROM Students;`;
